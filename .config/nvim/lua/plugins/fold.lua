8b4d64a095f98ffd3468e29bfd91fe186ec81287
niba <kamil.nibisz@gmail.com>
2025-09-23 08:58:28 +0200

    feat: fold changes (lsp folds are not very reliable)

--- .config/nvim/lua/plugins/fold.lua
+++ .config/nvim/lua/plugins/fold.lua
@@ -2,7 +2,7 @@ return {
   {
     -- sets folding to manual and breaks lsp / treesitter folds
     "dmtrKovalenko/fold-imports.nvim",
-    enabled = false,
+    enabled = true,
     opts = {
       custom_fold_text = false,
       fold_level = 99,
@@ -12,10 +12,22 @@ return {
   {
     "chrisgrieser/nvim-origami",
     enabled = true,
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+    config = function(_, opts)
+      require("origami").setup(opts)
+      vim.api.nvim_clear_autocmds({ group = "origami.foldexpr" })
+    end,
     opts = {
       foldtext = {
         padding = 2,
         lineCount = { template = "󰘖 %d" },
+      },
+      autoFold = {
+        enabled = false,
+        kinds = { "imports" },
       },
     },
     keys = {
